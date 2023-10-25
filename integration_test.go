2e88ac911458dae96b480c84c7f5d1318d837fbe
Connor Szczepaniak <cszczepaniak@gmail.com>
2023-10-24 22:19:00 -0500

    more integration-y tests

--- integration_test.go
+++ integration_test.go
@@ -1,66 +1,60 @@
 package main
 
 import (
-	"io"
 	"os"
+	"os/exec"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-type testWriter struct {
-	*strings.Builder
+func installBinary(t testing.TB) {
+	t.Helper()
+
+	sync.OnceFunc(func() {
+		_, err := exec.Command(`go`, `install`).CombinedOutput()
+		require.NoError(t, err)
+	})()
 }
 
-func (tw testWriter) Close() error {
-	return nil
-}
+func runCmd(t testing.TB, name string, args ...string) string {
+	installBinary(t)
 
-func newTestWriter() (func(string) (io.WriteCloser, error), *strings.Builder) {
-	sb := &strings.Builder{}
-	tw := testWriter{
-		Builder: sb,
-	}
+	stdout := &strings.Builder{}
+	stderr := &strings.Builder{}
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
 
-	return func(s string) (io.WriteCloser, error) {
-		return tw, nil
-	}, sb
+	err := cmd.Run()
+	require.NoError(t, err, stderr.String())
+
+	return stdout.String()
 }
 
 func TestPatch(t *testing.T) {
 	t.Run(`with patch`, func(t *testing.T) {
-		getWriter, sb := newTestWriter()
-
-		listSink := &strings.Builder{}
-
-		err := handlePatch(`testdata/sample.diff`, getWriter, listSink)
-		require.NoError(t, err)
-
-		assertMatchesFileContents(t, `testdata/test_with_diff_exp.go`, sb.String())
-		assert.Equal(t, []string{`testdata/test_with_diff.go`}, strings.Split(strings.TrimSpace(listSink.String()), "\n"))
+		out := runCmd(t, `go-pedantry`, `-patch`, `testdata/sample.diff`)
+		assertMatchesFileContents(t, `testdata/test_with_diff_exp.go`, out)
 	})
 
 	t.Run(`same file without patch`, func(t *testing.T) {
-		listSink := &strings.Builder{}
-
-		formatted, err := formatFile(`testdata/test_with_diff.go`, allNodes, listSink)
-		require.NoError(t, err)
-
-		assertMatchesFileContents(t, `testdata/test_without_diff_exp.go`, formatted)
-		assert.Equal(t, []string{`testdata/test_with_diff.go`}, strings.Split(strings.TrimSpace(listSink.String()), "\n"))
+		out := runCmd(t, `go-pedantry`, `-input`, `testdata/test_with_diff.go`)
+		assertMatchesFileContents(t, `testdata/test_without_diff_exp.go`, out)
 	})
 }
 
 func TestFormat(t *testing.T) {
-	listSink := &strings.Builder{}
+	out := runCmd(t, `go-pedantry`, `-input`, `testdata/complex.go`)
+	assertMatchesFileContents(t, `testdata/complex_exp.go`, out)
+}
 
-	formatted, err := formatFile(`testdata/complex.go`, allNodes, listSink)
-	require.NoError(t, err)
-
-	assertMatchesFileContents(t, `testdata/complex_exp.go`, formatted)
-	assert.Equal(t, []string{`testdata/complex.go`}, strings.Split(strings.TrimSpace(listSink.String()), "\n"))
+func TestList(t *testing.T) {
+	out := runCmd(t, `go-pedantry`, `-input`, `testdata`, `-l`)
+	assert.Equal(t, "testdata/complex.go\ntestdata/test_with_diff.go\ntestdata/test_with_diff_exp.go\n", out)
 }
 
 func assertMatchesFileContents(t testing.TB, expFile string, actContents string) {
